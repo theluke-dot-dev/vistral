91289f6f9b037ffb30df7fecd97faf1b58b32c02
Łukasz Wolak <lukasz.wolak@theluke.dev>
2023-09-17 00:26:46 +0200

    chore(command-bus,tests): minor changes in typing

--- src/vistral/command_bus/command.py
+++ src/vistral/command_bus/command.py
@@ -2,6 +2,8 @@ from abc import ABC, abstractmethod
 from dataclasses import dataclass
 from typing import Generic, TypeVar
 
+from typing_extensions import TypeAlias
+
 
 @dataclass(frozen=True)
 class Command:
@@ -15,3 +17,8 @@ class CommandHandler(Generic[TCommand], ABC):
     @abstractmethod
     def __call__(self, command: TCommand) -> None:
         pass
+
+
+CommandType: TypeAlias = type[Command]
+BoundCommandHandler: TypeAlias = CommandHandler[TCommand]
+BoundCommandHandlerType: TypeAlias = type[BoundCommandHandler]
